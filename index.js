c11e7a34bca24ca17d3ee554df95c6cc95348355
eb247 <eb247@hotmail.com>
2018-11-17 21:53:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,7 +78,8 @@ function removeFromCart(item) {
 
   if(removeItem.includes(item)){
     var a = removeItem.indexOf(item)
-    cart.splice(cart[a].itemName, 1)
+    console.log(cart[a].itemName)
+    //cart.splice(cart[a].itemName, 1)
   } else {
     return "That item is not in your cart."
   }
